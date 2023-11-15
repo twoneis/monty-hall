7953f7f4c0d372d8c00ce5e3373ab6c5ea7b2092
twoneis <sanjay29@sapura.de>
2023-11-15 10:19:02 +0100

    added flake

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "A flake for development with zig.";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in {
+      devShell.x86_64-linux = pkgs.mkShell {
+        name = "zig-devel";
+        packages = with pkgs; [
+          zig
+        ];
+        shellHook = ''export PS1="[\u@zig:\W]\$ "'';
+      };
+    };
+}
